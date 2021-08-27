9477bb2ecbfe3e112337f1c7a92623a21f703f22
Sagarika Shinde <sagarika.shinde@morningstar.com>
2021-08-28 00:29:36 +0530

    [sagarika]: Added Uc3 email pattern

--- src/com/userregistration/UserRegistration.java
+++ src/com/userregistration/UserRegistration.java
@@ -6,12 +6,12 @@ public class UserRegistration {
 
 	public static void main(String[] args) {
 		Scanner sc =  new Scanner(System.in);
-		System.out.println("1.First Name 2.Last Name");
+		System.out.println("1.First Name 2.Last Name 3.Email");
 		int option = sc.nextInt();
 
 		switch(option) {
 			case 1:
-				System.out.println("Validate first name");
+				System.out.println("Validate first name:");
 				String dummy = sc.nextLine();
 				String firstname = sc.nextLine();
 				String str1 = "[A-Z a-z]{3,}";
@@ -24,7 +24,7 @@ public class UserRegistration {
 				break;
 				
 			case 2:
-				System.out.println("Validate last name");
+				System.out.println("Validate last name:");
 				String dummy1 = sc.nextLine();
 				String lastname = sc.nextLine();
 				String str2 = "[A-Z a-z]{3,}";
@@ -34,6 +34,19 @@ public class UserRegistration {
 				else {
 					System.out.println("Enter valid name");
 				}
+			case 3:
+				System.out.println("Validate email:");
+				//String dummy2 = sc.nextLine();
+				String email = sc.next();
+				String str3 = "^([a-z0-9]{3,}[\\.-]?[a-z0-9]{3,})@([a-z\\d]{3,})[\\.]?([a-z]{2})$";;
+				if (email.matches(str3)) {
+					System.out.println("Valid");
+				}
+				else {
+					System.out.println("Enter email");
+				}
+				
+				
 		}
 		
 	}
