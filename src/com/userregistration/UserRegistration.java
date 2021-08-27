dac09f8f84478c287f8377266511d8e3945f810c
Sagarika Shinde <sagarika.shinde@morningstar.com>
2021-08-27 23:23:46 +0530

    [sagarika]: Added UC1 & UC2 for first and last name

--- src/com/userregistration/UserRegistration.java
+++ src/com/userregistration/UserRegistration.java
@@ -1,12 +1,40 @@
 package com.userregistration;
 
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class UserRegistration {
 
 	public static void main(String[] args) {
 		Scanner sc =  new Scanner(System.in);
+		System.out.println("1.First Name 2.Last Name");
+		int option = sc.nextInt();
+
+		switch(option) {
+			case 1:
+				System.out.println("Validate first name");
+				String dummy = sc.nextLine();
+				String firstname = sc.nextLine();
+				String str1 = "[A-Z a-z]{3,}";
+				if (firstname.matches(str1)) {
+					System.out.println("Valid");
+				}
+				else {
+					System.out.println("Enter valid name");
+				}
+				break;
+				
+			case 2:
+				System.out.println("Validate last name");
+				String dummy1 = sc.nextLine();
+				String lastname = sc.nextLine();
+				String str2 = "[A-Z a-z]{3,}";
+				if (lastname.matches(str2)) {
+					System.out.println("Valid");
+				}
+				else {
+					System.out.println("Enter valid name");
+				}
+		}
+		
 	}
 }
