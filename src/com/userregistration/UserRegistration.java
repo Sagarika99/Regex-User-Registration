cbf03f818d3062bff9deefd8f0af5490dda1dc06
Sagarika Shinde <sagarika.shinde@morningstar.com>
2021-08-27 22:59:31 +0530

    First Commit

---
+++ src/com/userregistration/UserRegistration.java
@@ -0,0 +1,12 @@
+package com.userregistration;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+
+	public static void main(String[] args) {
+		Scanner sc =  new Scanner(System.in);
+	}
+}
